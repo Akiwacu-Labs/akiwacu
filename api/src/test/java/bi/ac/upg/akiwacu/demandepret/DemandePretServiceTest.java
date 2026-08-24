4107143f38b25e7a13d85bc29c62cb45d845a335
Gloria <muhimpundugloria0@gmail.com>
2026-08-24 14:30:02 +0200

    fix(test): import cycle date type

--- api/src/test/java/bi/ac/upg/akiwacu/demandepret/DemandePretServiceTest.java
+++ api/src/test/java/bi/ac/upg/akiwacu/demandepret/DemandePretServiceTest.java
@@ -24,6 +24,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
